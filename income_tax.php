df65ed448443a5e2b380510716d8bdf92e79e566
JHNguyen801 <32757548+JHNguyen801@users.noreply.github.com>
2023-03-14 23:30:13 -0600

    Update income_tax.php

--- income_tax.php
+++ income_tax.php
@@ -1,6 +1,4 @@
 <?php
-// source: https://turbotax.intuit.com/tax-tips/irs-tax-return/current-federal-tax-rate-schedules/L7Bjs1EAD?srqs=null&cid=ppc_gg_nb_stan_all_SMVtest_Calculator-CalculatorEstimateBrackets-CalculatorEstimateBrackets-Exact_ty22-bu3-sb160_638115183966_147596029418_kwd-339132207516&srid=Cj0KCQiAgaGgBhC8ARIsAAAyLfH-Euk-3YBdDGXdAG5CY2kNcN8XW7axhkhIUVrfRgY2ecdgxz8ji7waAmqYEALw_wcB&targetid=kwd-339132207516&skw=2023%20tax%20brackets&adid=638115183966&ven=gg&gclid=Cj0KCQiAgaGgBhC8ARIsAAAyLfH-Euk-3YBdDGXdAG5CY2kNcN8XW7axhkhIUVrfRgY2ecdgxz8ji7waAmqYEALw_wcB&gclsrc=aw.ds
-// https://replit.com/@HoangNguyen198/IncomeTaxBracket?v=1
 
 define(
   'TAX_RATES',
